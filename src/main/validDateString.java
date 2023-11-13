6dc4ab9bb401f21141fdf82cb4848cb880de9a40
40360770 <syalcin01@qub.ac.uk>
2023-11-13 13:36:18 +0000

    Added new method

--- src/main/validDateString.java
+++ src/main/validDateString.java
@@ -6,5 +6,36 @@ public class validDateString {
 		// TODO Auto-generated method stub
 
 	}
+	
+	public boolean validSentence(String sentence) {
+		int quoteCounter = 0;
+		if(!Character.isUpperCase(sentence.charAt(0))) {
+			return false;
+		}
+		
+		switch(sentence.charAt(sentence.length()-1)) {
+		case '.': break; 
+		case '?' : break; 
+		case '!' : break; 
+		default: return false;
+		}
+		
+		
+		/**
+		 * Starting at 1 as we have already validated the first character as well as checking the last character
+		 */
+		for(int i = 1; i< sentence.length()-1; i++) {
+			
+			if(sentence.charAt(i)== '"') {
+				quoteCounter++; 
+				
+			}
+		}
+		
+
+		
+		return true; 
+		
+	}
 
 }

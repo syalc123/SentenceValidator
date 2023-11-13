6d19f16fec9fbdd1a4bde9de8ca5fefa1028a137
40360770 <syalcin01@qub.ac.uk>
2023-11-13 14:44:49 +0000

    testing out valid Sentence method

--- src/main/validDateString.java
+++ src/main/validDateString.java
@@ -5,11 +5,13 @@ public class validDateString {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		String s = "Are there 11, 12, or 13 lazy dogs?";
+		System.out.println(validSentence(s));
 	}
 	
-	public boolean validSentence(String sentence) {
+	public static boolean validSentence(String sentence) {
 		int quoteCounter = 0; // variable to keep track of all quotation marks
-		
+		int digitCheck = 0; // variable to keep track of any digits in the sentence
 		String[] mys = sentence.split(" ");
 		
 		switch(sentence.charAt(sentence.length()-1)) {
@@ -20,10 +22,17 @@ public class validDateString {
 		}
 		
 		for(String x : mys) {
+			if(x.contains(",") == true)
+			try {
+				 digitCheck = Integer.parseInt(x);
+			}catch(Exception ex) {
+				ex.toString(); 
+			}
 			
+			if(digitCheck > 0 && digitCheck<=13) {
+				return false;
+			}
 		}
-		
-		
 		
 		/**
 		 * Starting at 1 as we have already validated the first character as well as checking the last character
@@ -33,19 +42,13 @@ public class validDateString {
 			if(sentence.charAt(i)== '"') {
 				quoteCounter++; 
 			}
-			
-			if(sentence.charAt(i) == '.') {
+			if(sentence.charAt(i) == '.') { 
 				return false; 
-			}
 			
+			}
 		}
 		
 		return quoteCounter %2 == 0 && Character.isUpperCase(sentence.charAt(0)); 
-		
-		
-
-		
-		
 		
 	}
 

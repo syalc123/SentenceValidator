9d2b3aefde6222982859ec818499b97bfa1fd708
40360770 <syalcin01@qub.ac.uk>
2023-11-13 16:13:05 +0000

    Added test cases

--- src/JUnitTests/validSentenceTest.java
+++ src/JUnitTests/validSentenceTest.java
@@ -1,17 +1,50 @@
 package JUnitTests;
 
+import main.SentenceValidator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class validSentenceTest {
 
-	public static void main(String[] args) {
-		
-		
-		
-		
+	String testValue;
+	boolean testResult;
+
+	// Given that the first letter is capital but all other conditions are met. Then
+	// return false
+	@Test
+	void testFirstLetterCapital() {
+		testValue = "first one \"\" sentence.";
+		testResult = SentenceValidator.validSentence(testValue);
+		assertEquals(testResult, false);
+
 	}
-	
-	
-	
-	
+
+	// Given that the last letter is not punctuation but all other conditions = true 
+	// Then return false
+	@Test
+	void testLastCharPunctuation() {
+		testValue = "Second two \"\" sentence";
+		testResult = SentenceValidator.validSentence(testValue);
+		assertEquals(testResult, false);
+	}
+
+	// Given that their are unclosed quotations. But all other conditions are met
+	// Then return false
+	@Test
+	void testUnclosedQuotes() {
+		testValue = "Second two \" sentence";
+		testResult = SentenceValidator.validSentence(testValue);
+		assertEquals(testResult, false);
+	}
+
+	// Given that a negative number is entered quotations. But all other conditions are met
+	// Then return false
+	@Test
+	void testNegativeNumberEntered() {
+		testValue = "Second two \" sentence";
+		testResult = SentenceValidator.validSentence(testValue);
+		assertEquals(testResult, false);
+	}
 }
